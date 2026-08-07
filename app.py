f980b44180f27c16ae52ccd2e1d1ed58d5ed03cc
~Rohit~ <rohitdas8769@gmail.com>
2026-08-07 23:41:58 +0530

    Initial commit - Movie Recommender System

---
+++ app.py
@@ -0,0 +1,109 @@
+import pickle
+import streamlit as st
+import requests
+
+
+# -----------------------------
+# Fetch movie poster from TMDB
+# -----------------------------
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=b6e74b622cce0a6c4a41e5193c94f20d&language=en-US"
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+    data = response.json()
+
+    poster_path = data.get("poster_path")
+
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+
+# -----------------------------
+# Recommend Movies
+# -----------------------------
+def recommend(movie):
+
+    index = movies[movies['title'] == movie].index[0]
+
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:
+
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movie_names.append(
+            movies.iloc[i[0]].title
+        )
+
+        recommended_movie_posters.append(
+            fetch_poster(movie_id)
+        )
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+# -----------------------------
+# Streamlit UI
+# -----------------------------
+
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+
+st.title("🎬 Movie Recommender System")
+
+
+# Load pickle files
+# Load pickle files
+
+movies = pickle.load(open("movie_dict (1).pkl", "rb"))
+similarity = pickle.load(open("similarity (1).pkl", "rb"))
+
+# Debug information
+st.write("Movies shape:", movies.shape)
+st.write("Similarity rows:", len(similarity))
+st.write("Similarity columns:", len(similarity[0]))
+
+movie_list = movies['title'].values
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movie_list
+)
+
+if st.button("Recommend"):
+
+    names, posters = recommend(selected_movie)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.image(posters[0])
+        st.write(names[0])
+
+    with col2:
+        st.image(posters[1])
+        st.write(names[1])
+
+    with col3:
+        st.image(posters[2])
+        st.write(names[2])
+
+    with col4:
+        st.image(posters[3])
+        st.write(names[3])
+
+    with col5:
+        st.image(posters[4])
+        st.write(names[4])
